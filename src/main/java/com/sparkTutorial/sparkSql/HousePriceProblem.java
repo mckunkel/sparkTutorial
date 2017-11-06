cbe242fd2c20788425a26e4982a7bef6357c8b0a
mckunkel <mkunkel@jlab.org>
2017-11-06 18:49:16 +0100

    upload?

--- src/main/java/com/sparkTutorial/sparkSql/HousePriceProblem.java
+++ src/main/java/com/sparkTutorial/sparkSql/HousePriceProblem.java
@@ -1,41 +1,68 @@
 package com.sparkTutorial.sparkSql;
 
+import static org.apache.spark.sql.functions.avg;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.max;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 public class HousePriceProblem {
 
-        /* Create a Spark program to read the house data from in/RealEstate.csv,
-           group by location, aggregate the average price per SQ Ft and max price, and sort by average price per SQ Ft.
+	/*
+	 * Create a Spark program to read the house data from in/RealEstate.csv,
+	 * group by location, aggregate the average price per SQ Ft and max price,
+	 * and sort by average price per SQ Ft.
+	 * 
+	 * The houses dataset contains a collection of recent real estate listings 
+	 * in San Luis Obispo county and around it. 
+	 * 
+	 * The dataset contains the following fields:
+	 * 1. MLS: Multiple listing service number for the house (unique ID).
+	 * 2. Location: city/town where the house is located. Most locations are in 
+	 * San Luis Obispo county and
+	 * northern Santa Barbara county (Santa Maria­Orcutt, Lompoc, Guadelupe, Los
+	 *  Alamos), but there some out of area locations as well.
+	 * 3. Price: the most recent listing price of the house (in dollars).
+	 * 4. Bedrooms: number of bedrooms. 5. Bathrooms: number of bathrooms.
+	 * 6. Size: size of the house in square feet.
+	 * 7. Price/SQ.ft: price of the house per square foot.
+	 * 8. Status: type of sale. Thee types are represented in the dataset: Short
+	 *  Sale, Foreclosure and Regular.
+	 * 
+	 * Each field is comma separated.
+	 * 
+	 * Sample output:
+	 * 
+	 * +----------------+-----------------+----------+ | Location| avg(Price SQ
+	 * Ft)|max(Price)| +----------------+-----------------+----------+ | Oceano|
+	 * 1145.0| 1195000| | Bradley| 606.0| 1600000| | San Luis Obispo| 459.0|
+	 * 2369000| | Santa Ynez| 391.4| 1395000| | Cayucos| 387.0| 1500000|
+	 * |.............................................|
+	 * |.............................................|
+	 * |.............................................|
+	 * 
+	 */
 
-        The houses dataset contains a collection of recent real estate listings in San Luis Obispo county and
-        around it. 
+	public static void main(String[] args) {
+		Logger.getLogger("org").setLevel(Level.ERROR);
+		SparkSession session = SparkSession.builder().appName("StackOverFlowSurvey").master("local[1]").getOrCreate();
 
-        The dataset contains the following fields:
-        1. MLS: Multiple listing service number for the house (unique ID).
-        2. Location: city/town where the house is located. Most locations are in San Luis Obispo county and
-        northern Santa Barbara county (Santa Maria­Orcutt, Lompoc, Guadelupe, Los Alamos), but there
-        some out of area locations as well.
-        3. Price: the most recent listing price of the house (in dollars).
-        4. Bedrooms: number of bedrooms.
-        5. Bathrooms: number of bathrooms.
-        6. Size: size of the house in square feet.
-        7. Price/SQ.ft: price of the house per square foot.
-        8. Status: type of sale. Thee types are represented in the dataset: Short Sale, Foreclosure and Regular.
+		Dataset<Row> responses = session.read().option("header", "true").csv("in/RealEstate.csv");
 
-        Each field is comma separated.
+		System.out.println("=== Print out schema ===");
+		responses.printSchema();
 
-        Sample output:
+		Dataset<Row> castedResponse = responses.withColumn("Price SQ Ft", col("Price SQ Ft").cast("long"))
+				.withColumn("Price", col("Price").cast("long"));
+		castedResponse.printSchema();
 
-        +----------------+-----------------+----------+
-        |        Location| avg(Price SQ Ft)|max(Price)|
-        +----------------+-----------------+----------+
-        |          Oceano|           1145.0|   1195000|
-        |         Bradley|            606.0|   1600000|
-        | San Luis Obispo|            459.0|   2369000|
-        |      Santa Ynez|            391.4|   1395000|
-        |         Cayucos|            387.0|   1500000|
-        |.............................................|
-        |.............................................|
-        |.............................................|
+		castedResponse.groupBy(col("Location")).agg(avg("Price SQ Ft"), max(col("Price")))
+				.orderBy(col("avg(Price SQ Ft)").desc()).show();
 
-         */
+		session.stop();
+	}
 }
